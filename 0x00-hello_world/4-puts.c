92997d611ba46f785b6f2b38e3ac2e2777c6c245
kyle-gross <kyg555@outlook.com>
2021-02-03 20:56:28 +0000

    Fixed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	printf("\"Programming is like building a multilingual puzzle\n");
+	char str1[53];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+
+	puts(str1);
+
 	return(0);
 }
