5608afda409568aa31a0d68b9735b2a1eedca14c
kyle-gross <kyg555@outlook.com>
2021-02-03 21:42:28 +0000

    Completed task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - uses printf()
+ *
+ * Prints a string using the printf() function
+ * Return: 0
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
