a98bea7cceb10f21ee8f7f7b9a145e2408ae334d
kyle-gross <kyg555@outlook.com>
2021-02-04 20:50:33 +0000

    Complete task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+
+	return (0);
+}
