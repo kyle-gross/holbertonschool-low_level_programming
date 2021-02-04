38fcad4f450017e34699e6ba667cb53090986ba3
kyle-gross <kyg555@outlook.com>
2021-02-04 16:11:11 +0000

    Fix task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 
-	write(1, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 
 	return (1);
 
