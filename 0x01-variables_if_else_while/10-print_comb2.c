9f5fe6bc3b88423e3f10a42ada0f212e3c1124a6
kyle-gross <kyg555@outlook.com>
2021-02-04 23:15:11 +0000

    solve task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints 00 - 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, i;
+
+	n = 48;
+	while (n < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			putchar(n);
+			putchar(i);
+			if (n == 57 && i == 57)
+				break;
+			i++;
+			putchar(',');
+			putchar(' ');
+			
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
