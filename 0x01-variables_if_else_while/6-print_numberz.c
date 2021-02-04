9c75d31fcaaf1b24fe2064c814b4e5a24525e0f5
kyle-gross <kyg555@outlook.com>
2021-02-04 21:05:21 +0000

    Complete task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9 using only putchar()
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
