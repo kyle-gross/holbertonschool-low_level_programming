7e4a8572b5b2e610b753450c2941edc512e370dd
kyle-gross <kyg555@outlook.com>
2021-02-08 23:53:40 +0000

    Updated header, added task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - checks if int r is lower or uppercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	putchar(r + '0');
+	r = _islower('o');
+	putchar(r + '0');
+	r = _islower(108);
+	putchar(r + '0');
+	putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <unistd.h>
 #include <stdio.h>
-#ifndef holberton
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_alphabet - prints alphabet to stdout
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	char c;
@@ -10,6 +30,11 @@ void print_alphabet(void)
 	putchar('\n');
 }
 
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: void
+ */
 void print_alphabet_x10(void)
 {
 	int c;
@@ -17,5 +42,19 @@ void print_alphabet_x10(void)
 	for (c = 0; c < 10; c++)
 		print_alphabet();
 }
-#define holberton
+
+/**
+ * _islower - determines if character is uppercase or lowercase
+ * @c: The character to test
+ *
+ * Return: 0 if true, 1 if false
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+#define HOLBERTON_H
 #endif
