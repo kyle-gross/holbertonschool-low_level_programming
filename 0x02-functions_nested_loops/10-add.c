8962d6ee3adb1b81ad6cf9aae6b944cb4f26deab
kyle-gross <kyg555@outlook.com>
2021-02-09 19:18:40 +0000

    Complete task 10, update .h file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - adds 2 numbers
+ * @i: number to be added
+ * @n: number to be added
+ *
+ * Return: the value of i + n
+ */
+
+int add(int i, int n)
+{
+	int add;
+
+	add = i + n;
+	return (add);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -10,4 +10,5 @@ int _abs(int i);
 int print_last_digit(int i);
 void jack_bauer(void);
 void times_table(void);
+int add(int i, int n);
 #endif
