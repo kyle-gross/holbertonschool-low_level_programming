563bf53af84848fd118316adc3410ad0dfc03b4f
kyle-gross <kyg555@outlook.com>
2021-02-09 06:46:29 +0000

    Fix task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,16 +1,20 @@
 #include "holberton.h"
-#include <string.h>
 /**
  * main - prints word "Holberton"
+ *
  * Return: 0
  */
 int main(void)
 {
-	char *c = "Holberton";
-	unsigned int i;
-
-	for (i = 0; i < strlen(c); i++)
-		_putchar(c[i]);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
