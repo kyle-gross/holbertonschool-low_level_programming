6272ae94c0d4c72e3a12e46dd0297ed352a6e011
kyle-gross <kyg555@outlook.com>
2021-02-09 07:28:33 +0000

    Add task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _isalpha - determines if character is a letter
+ * @c: The character to test
+ *
+ * Return: 1 if true, 0 if false
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
