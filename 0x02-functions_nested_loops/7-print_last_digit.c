dc3249b2d9c679816c2c22c7e1b1e8ad549077ee
kyle-gross <kyg555@outlook.com>
2021-02-09 19:13:26 +0000

    Added _abs function; reformat output

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,33 +2,34 @@
 #include <stdio.h>
 
 /**
+ * _abs - prints absolute value of a number
+ * @i: The number to test absolute value of
+ *
+ * Return: 0 if i == 0, i if i > 0, or -i if i < 0
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+		return (i);
+	else if (i == 0)
+		return (0);
+	else
+		return (-i);
+}
+
+/**
  * print_last_digit - prints last digit of a number
  * @i: The number to find last digit of
  *
- * Return: n - the last digit
+ * Return: l - the last digit
  */
 
 int print_last_digit(int i)
 {
 	int l;
 
-	if (i < 0)
-	{
-		i *= -1;
-		l = i % 10;
-		_putchar(l + 48);
-		return (l);
-	}
-	else if (i > 0)
-	{
-		l = i % 10;
-		_putchar(l + 48);
-		return (l);
-	}
-	else
-	{
-		l = 0;
-		_putchar(l + 48);
-		return (0);
-	}
+	l = _abs((i % 10));
+	_putchar(l + 48);
+	return (l);
 }
