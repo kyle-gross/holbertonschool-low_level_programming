10509f225e471a8d196a7ec08bd734c238d31337
kyle-gross <kyg555@outlook.com>
2021-02-09 15:27:31 +0000

    Update doc

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 
 /**
+ * print_last_digit - prints last digit of a number
+ * @r: The number to find last digit of
  *
+ * Return: n - the last digit
  */
 
-int print_last_digit(int i)
+int print_last_digit(int r)
 {
 	int n;
 
-	n = i % 10;
+	n = r % 10;
 
 	return (n);
 }
