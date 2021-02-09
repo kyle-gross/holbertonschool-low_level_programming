a6973632cbc21448b8dea81af6bd18f2fb8d740c
kyle-gross <kyg555@outlook.com>
2021-02-09 17:27:57 +0000

    Fixed location of break

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ *
+ */
+
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 9; h2++)
+		{
+			if (h1 == 2 && h2 == 4)
+				break;
+
+			for (m1 = 0; m1 <= 5; m1++)
+			{
+				for (m2 = 0; m2 <= 9; m2++)
+				{
+					_putchar(h1 % 10 + '0');
+					_putchar(h2 % 10 + '0');
+					_putchar(':');
+					_putchar(m1 % 10 + '0');
+					_putchar(m2 % 10 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
