dc80a0e4ef1624ad14773ae1d98c4558f1b09d98
kyle-gross <kyg555@outlook.com>
2021-02-10 01:13:48 +0000

    Changed Feb. 29 conditions

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,11 +16,11 @@ void print_remaining_days(int month, int day, int year)
 	if (year % 4 == 0 || year % 400 == 0)
 	{
 		/* If February 29 */
-/*		if (month >= 2 && day >= 60)
- *		{
- *			day++;
- *		}
- */		printf("Day of the year: %d\n", day);
+		if (month == 2 && day == 60)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
