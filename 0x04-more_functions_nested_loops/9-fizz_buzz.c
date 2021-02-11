1d490992a11be90b2aec815c5d140878b119aa46
kyle-gross <kyg555@outlook.com>
2021-02-11 18:52:25 +0000

    Add condition to remove trailing space if number == 100

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,11 +15,26 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+		{
+			if (i < 100)
+				printf("FizzBuzz ");
+			else
+				printf("FizzBuzz");
+		}
 		else if (i % 3 == 0)
-			printf("Fizz ");
+		{
+			if (i < 100)
+				printf("Fizz ");
+			else
+				printf("Fizz");
+		}
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (i < 100)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		}
 		else
 			printf("%d ", i);
 	}
