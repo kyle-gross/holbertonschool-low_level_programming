d070ff32e2d38e1e68d97878159e74cf31e8f6aa
kyle-gross <kyg555@outlook.com>
2021-02-11 02:52:11 +0000

    Now printing sum instead of numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,21 +9,17 @@
 int main(void)
 {
 	int i;
-	long int n, n1 = 1, n2 = 2;
+	long int n, n1 = 1, n2 = 2, sum = 0;
 
 	for (i = 0; i < 32; i++)
 	{
 		if (n1 % 2 == 0)
-		{
-			printf("%lu", n1);
-			if (i < 31)
-				printf(", ");
-		}
+			sum += n1;
 		n = n1 + n2;
 		n1 = n2;
 		n2 = n;
 	}
-	putchar('\n');
+	printf("%lu\n", sum);
 
 	return (0);
 }
