794b98ef46b1be16bc21b482b687337627568caf
kyle-gross <kyg555@outlook.com>
2021-02-12 00:58:57 +0000

    Fixed int min error

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,12 +9,17 @@
 
 void print_number(int n)
 {
+	char c = '0';
+
 	if (n < 0)
 	{
 		_putchar('-');
+		n++;
+		c = '1';
 		n = -n;
 	}
+
 	if (n / 10)
 		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	_putchar(n % 10 + c);
 }
