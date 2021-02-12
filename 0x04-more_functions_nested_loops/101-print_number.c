74d47ccaadc13f99a9542cdc475e7e9a5fc69965
kyle-gross <kyg555@outlook.com>
2021-02-12 00:25:25 +0000

    Changed variable name

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,28 +7,28 @@
  *Return: void
  */
 
-void print_number(int i)
+void print_number(int n)
 {
-	if (i / 10 != 0)
+	if (n / 10 != 0)
 	{
-		printnumber(i / 10);
-		if (i > 0)
+		printnumber(n / 10);
+		if (n > 0)
 		{
-			_putchar(i % 10 + '0');
+			_putchar(n % 10 + '0');
 		}
 		else
 		{
-			_putchar(-i % 10 + '0');
+			_putchar(-n % 10 + '0');
 		}
 	}
-	else if ((i / 10 == 0) && (i % 10 != 0) && (i > 0))
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
 	{
-		_putchar(i % 10 + '0');
+		_putchar(n % 10 + '0');
 	}
-	else if ((i / 10 == 0) && (i % 10 != 0) && (i <= 0))
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
 	{
 		_putchar('-');
-		_putchar(-i % 10 + '0');
+		_putchar(-n % 10 + '0');
 	}
 	else
 		_putchar('0');
