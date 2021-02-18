ec39281865679d9529e68d75db456679fe7772d1
kyle-gross <kyg555@outlook.com>
2021-02-18 18:29:52 +0000

    Added for loop to append null byte

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,12 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
-	while (src[i] != '\0' && i < n)
-	{
-	*(dest + i) = *(src + i);
-	i++;
-	}
-	*(dest + i) = '\0';
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
