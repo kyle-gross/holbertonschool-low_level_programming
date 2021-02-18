50ca4ddda150df53965c407a2ba3fd4d61cd75c2
kyle-gross <kyg555@outlook.com>
2021-02-18 21:07:32 +0000

    String to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - converts a string to uppercase
+ * @*s: string to convert
+ *
+ * Return: Uppercase string
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 97 && *(s + i) <= 122)
+			*(s + i) = *(s + i) - 32;
+		i++;
+	}
+	return (s);
+}
