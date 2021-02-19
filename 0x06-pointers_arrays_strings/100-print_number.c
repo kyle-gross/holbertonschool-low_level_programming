2fa78e135c2e686055a18636c8e27d4d4319c2da
kyle-gross <kyg555@outlook.com>
2021-02-19 00:57:44 +0000

    Commit task 9

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints a number
+ * @n: the number to print
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	char c = '0';
+
+	if (n < 0)
+	{
+		if (n % 10 == 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		else
+		{
+			_putchar('-');
+			n++;
+			c = '1';
+			n = -n;
+		}
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + c);
+}
