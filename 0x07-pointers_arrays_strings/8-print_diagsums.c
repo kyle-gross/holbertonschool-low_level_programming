f6593a86dc8211a1424ad86246c3725b1028a7dc
kyle-gross <kyg555@outlook.com>
2021-02-23 00:04:32 +0000

    Added comments

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,13 +13,16 @@ void print_diagsums(int *a, int size)
 	/* Variables */
 	int i, sum1 = 0, sum2 = 0;
 
-	/* Loop through array - add diagonals to sum1 */
+	/* Loop through array from top left to bottom right */
 	for (i = 0; i < size * size; i += size + 1)
 	{
+		/* Add to sum1 */
 		sum1 += a[i];
 	}
+	/* Loop through array from top right to bottom left */
 	for (i = size - 1; i < size * size - size + 1; i += size - 1)
 	{
+		/* Add to sum2 */
 		sum2 += a[i];
 	}
 	printf("%d, %d\n", sum1, sum2);
