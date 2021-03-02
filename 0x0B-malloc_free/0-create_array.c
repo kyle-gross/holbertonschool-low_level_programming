3d517c04a564655703b470e5b78b375dec992f53
kyle-gross <kyg555@outlook.com>
2021-03-02 18:34:40 +0000

    Add condition for NULL

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *s = (char *)malloc(size);
+	char *s;
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
+
+	s = (char *)malloc(size);
+
+	if (s == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		s[i] = c;
+
 	return (s);
 }
