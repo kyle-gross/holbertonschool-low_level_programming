84ae4dcab0e17787a1cca89f2802cc99aaefc5eb
kyle-gross <kyg555@outlook.com>
2021-03-02 18:36:13 +0000

    Commit task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - copies a string
+ * @str: the string to copy
+ * Return: NULL if string == NULL or pointer to copied string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i = 0;
+
+	if (str[0] == '\0')
+		return ('\0');
+
+	while (str[i])
+		i++;
+
+	dup = (char *)malloc(i);
+	i = 0;
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	return (dup);
+}
