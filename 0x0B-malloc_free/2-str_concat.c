11abb4364c92c2cecce617808ba30682ede40d62
kyle-gross <kyg555@outlook.com>
2021-03-02 19:59:21 +0000

    Add conditions for NULL strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,11 @@ char *str_concat(char *s1, char *s2)
 	int size1 = 0, size2 = 0, i = 0;
 	char *cat;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 
