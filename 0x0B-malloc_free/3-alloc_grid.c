968cd2227588dab67ed374401524bb9a59e98614
kyle-gross <kyg555@outlook.com>
2021-03-02 21:03:34 +0000

    Initial commit task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - creates 2d array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return: NULL if w or h is negative, or pointer to 2d array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i = 0, ii = 0;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = (int **)malloc(height * sizeof(int *));
+
+	for (; i < width; i++)
+		arr[i] = (int *)malloc(width * sizeof(int));
+
+	/* Initialize array to 0's */
+	for (; i < height; i++)
+	{
+		for (; ii < width; ii++)
+		{
+			arr[i][ii] = 0;
+		}
+	}
+
+	return (arr);
+}
