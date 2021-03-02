88a32480c557479451dff68fa35d4e0fd46ea9fb
kyle-gross <kyg555@outlook.com>
2021-03-02 21:36:45 +0000

    Fixed loop variable init

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *)malloc(width * sizeof(int));
 
 	/* Initialize array to 0's */
-	for (; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
 		for (; ii < width; ii++)
 		{
