27221b8eb91bfbbd5db56095eeea5714f44929f5
kyle-gross <kyg555@outlook.com>
2021-03-04 20:05:37 +0000

    Init strlen variable

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ * _strlen - gets length of string
+ * @s: the string
+ * Return: size of string
+ */
+unsigned int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
  * string_nconcat - concatenates two strings
  * @s1: string 1
  * @s2: string 2
@@ -9,35 +23,22 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr = NULL;
 	unsigned int i, ii;
+	char *ptr = NULL;
 
-	if (n >= (unsigned int)_strlen(s2))
-		n = (unsigned int)_strlen(s2);
-	ptr = malloc((unsigned int)_strlen(s1) * sizeof(char) + n + 2);
-	if (ptr == NULL)
-		return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	if (n >= _strlen(s2))
+		n = _strlen(s2);
+	ptr = malloc(_strlen(s1) * sizeof(char) + n + 2);
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0, ii = 0; s1[i] != '\0'; i++, ii++)
 		ptr[ii] = s1[i];
-	for (i = 0; i < n; i++, ii++)
+	for (i = 0; i <= n; i++, ii++)
 		ptr[ii] = s2[i];
 	ptr[ii] = '\0';
 	return (ptr);
-}
-/**
- * _strlen - gets length of string
- * @s: the string
- * Return: size of string
- */
-int _strlen(char *s)
-{
-	int i;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
 }
