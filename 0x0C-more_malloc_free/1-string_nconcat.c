8e6f60f0151be8bf05c1da125998ea6bf559b9a8
kyle-gross <kyg555@outlook.com>
2021-03-04 20:08:56 +0000

    Changed 2 to 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	if (n >= _strlen(s2))
 		n = _strlen(s2);
-	ptr = malloc(sizeof(char) * _strlen(s1) + n + 2);
+	ptr = malloc(sizeof(char) * _strlen(s1) + n + 1);
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0, ii = 0; s1[i] != '\0'; i++, ii++)
