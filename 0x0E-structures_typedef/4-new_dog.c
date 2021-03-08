0121ea616d703d8e16e0b1dbd2c68d922dc28241
kyle-gross <kyg555@outlook.com>
2021-03-08 23:59:39 +0000

    Removed last change

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,46 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strlen - counts length of a string
+ * @s: the string to count
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+/**
+ * _strdup - copies a string
+ * @str: the string to copy
+ * Return: NULL if string == NULL or pointer to copied string
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	dup = (char *)malloc(_strlen(str) + 1);
+
+	if (dup == NULL)
+		return (NULL);
+
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+/**
  * new_dog - creates a new dog
  * @name: name of new dog
  * @age: age of new dog
@@ -13,14 +53,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (dog2 == NULL)
 		return (NULL);
-	dog2->name = name;
+	dog2->name = _strdup(name);
 	if (dog2->name == NULL)
 	{
 		free(dog2);
 		return (NULL);
 	}
 	dog2->age = age;
-	dog2->owner = owner;
+	dog2->owner = _strdup(owner);
 	if (dog2->owner == NULL)
 	{
 		free(dog2->name);
