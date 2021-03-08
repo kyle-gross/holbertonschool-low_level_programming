11481360aca3e444f73c704c98bf0585a1e39fc8
kyle-gross <kyg555@outlook.com>
2021-03-08 21:07:23 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - name, age, and owner of dog
+ * @name: the dog's name
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
