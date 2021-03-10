1fe9c1f6cc3418a3032df7ddaeefa2676f43f10b
kyle-gross <kyg555@outlook.com>
2021-03-10 18:44:01 +0000

    Commit task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: function which accepts string data type
+ * Return: void
+ */
+void print_name(char *name, void(*f)(char *))
+{
+	if (name != NULL && name != "")
+	{
+		f(name);
+	}
+}
