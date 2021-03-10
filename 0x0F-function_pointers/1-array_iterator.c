adf820bde06d24b8eec7670ca4b982f2e6537ba2
kyle-gross <kyg555@outlook.com>
2021-03-10 19:09:50 +0000

    Commit task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array to execute on
+ * @size: size of array
+ * @action: pointer to function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (action != NULL && size > 0)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
+}
