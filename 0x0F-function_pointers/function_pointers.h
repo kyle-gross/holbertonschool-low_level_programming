54043c60065e4b2230a42d1903c7a91da357b3cd
kyle-gross <kyg555@outlook.com>
2021-03-10 18:51:07 +0000

    Edit if statement and update .h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_P_H
 #define FUNCTION_P_H
 
+#include <stdlib.h>
+
 /* Prototypes */
 void print_name(char *name, void (*f)(char*));
 void array_iterator(int *array, size_t size, void (*action)(int));
