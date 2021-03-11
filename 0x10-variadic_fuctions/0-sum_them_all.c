4ff49c04089b6e1000bdb3737cb51bd9d0198e6a
kyle-gross <kyg555@outlook.com>
2021-03-11 18:09:56 +0000

    Commit task 0

--- 0x10-variadic_fuctions/0-sum_them_all.c
+++ 0x10-variadic_fuctions/0-sum_them_all.c
@@ -1,4 +1,21 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
 /**
- *
+ * sum_them_all - sums all parameters
+ * @n:
+ * Return: Sum of parameters
  */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	int i = (int)n, sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(list, n);
+	for (; i; i--)
+		sum += va_arg(list, int);
+	va_end(list);
+	return (sum);
+}
