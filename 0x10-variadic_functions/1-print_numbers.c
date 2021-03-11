3b758a72d0b31e8848b4f1d27e6c260531b6917d
kyle-gross <kyg555@outlook.com>
2021-03-11 18:42:14 +0000

    Remove if statement

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,16 +13,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	if (n != 0)
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(ap, n);
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(ap, int));
-			if (separator != NULL && i != n - 1)
-				printf("%s", separator);
-		}
-		va_end(ap);
-		printf("\n");
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
 	}
+	va_end(ap);
+	printf("\n");
 }
