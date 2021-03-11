6853b89c546103b02714e444fd339a92c4ef3ce5
kyle-gross <kyg555@outlook.com>
2021-03-11 02:21:37 +0000

    Add more comments

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,31 +4,39 @@
  * main - prints opcodes
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ * Return: 0 if successful
  */
 int main(int argc, char *argv[])
 {
 	int i = 0, numbytes;
-	char *ptr = (char *)main;
+	char *ptr = (char *)main; /* Typecast main to match ptr */
 
+	/* If # of arguments incorrect */
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	/* If # of bytes is negative */
 	if (atoi(argv[1]) < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+	/* Convert from string to integer */
 	numbytes = atoi(argv[1]);
+	/* Loop through # of times specified by numbytes */
 	for (; i < numbytes; i++)
 	{
+		/* Print 2 characters in hexadecimal format */
 		printf("%02hhx", *ptr);
+		/* Add a space if it's not the last number */
 		if (i < numbytes - 1)
 			printf(" ");
-		ptr += 1;
+		/* Increment ptr */
+		ptr ++;
 	}
+	/* Add new line */
 	printf("\n");
 	return (0);
 }
