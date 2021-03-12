ca7680dee6e688107a1d101673ae1409809b3956
kyle-gross <kyg555@outlook.com>
2021-03-12 03:13:35 +0000

    Update .h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,6 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 /* Macros */
-#define COMMA_SPACE(i, ii) ((ii) < (i - 1) ? ", " : "") 
+#define COMMA_SPACE(s) ((s) > 0 ? ", " : "") 
 
 #endif
