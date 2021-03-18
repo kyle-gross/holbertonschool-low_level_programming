bd4badb4f1140b81acbc170c7eb018f343e5533c
kyle-gross <kyg555@outlook.com>
2021-03-18 18:29:01 +0000

    Add new dir

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+#Holberton School project for singly linked lists
+* An introduction to linked lists
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS
+#define LISTS
+
+/* INCLUDES */
+#include <stdlib.h>
+#include <unistd.h>
+
+/* PROTOTYPES */
+int _putchar(char c);
+
+#endif
