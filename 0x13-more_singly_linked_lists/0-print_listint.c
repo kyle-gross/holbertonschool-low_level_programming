26fa1eac5f59d4c75187457ae78b2e54db41a481
kyle-gross <kyg555@outlook.com>
2021-03-22 16:32:40 +0000

    Commit task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - prints a linked list
+ * @h: pointer to head of list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+		return (count);
+	while (h->next != NULL)
+	{
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	count++;
+	printf("%d\n", h->n);
+	return (count);
+}
