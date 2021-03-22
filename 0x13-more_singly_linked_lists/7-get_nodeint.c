38af7fa8b3f4a02205a49a47fa9aa506b823760f
kyle-gross <kyg555@outlook.com>
2021-03-22 20:44:29 +0000

    Fix index position

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (!head)
 		return (NULL);
 	temp = head;
-	while (temp_index <= index)
+	while (temp_index < index)
 	{
 		temp = temp->next;
 		if (temp_index + 1 < index && temp->next == NULL)
