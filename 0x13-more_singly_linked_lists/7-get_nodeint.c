caa511db7529395f432d7d55ac879c32251ea671
kyle-gross <kyg555@outlook.com>
2021-03-23 16:40:38 +0000

    Change format of 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,13 +12,34 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (!head)
 		return (NULL);
+	if (index >= listint_len(head))
+		return (NULL);
 	temp = head;
 	while (temp_index < index)
 	{
 		temp = temp->next;
-		if (temp_index + 1 < index && temp->next == NULL)
+/*		if (temp_index + 1 < index && temp->next == NULL)
 			return (NULL);
-		temp_index++;
+*/		temp_index++;
 	}
 	return (temp);
+}
+/**
+ * listint_len - returns the length of a linked list
+ * @h: pointer to head of list
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+		return (count);
+	while (h->next != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	count++;
+	return (count);
 }
