5d0dedee59d1c196b279581996fb5791a2504fa4
kyle-gross <kyg555@outlook.com>
2021-03-23 18:37:18 +0000

    Change while loop condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,7 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->n = n;
 		trav = *head;
-		while (temp_index < idx)
+		while (temp_index < idx - 1)
 		{
 			trav = trav->next;
 			temp_index++;
