29bc53f243dd14e875495e6cf75fee25790ddc01
kyle-gross <kyg555@outlook.com>
2021-03-23 06:05:27 +0000

    Add null check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
+	if (!*head)
+		return;
 	while (temp)
 	{
 		*head = temp;
 		temp = temp->next;
 		free(*head);
 	}
-	*head = (*head)->next;
-	free(*head);
 	*head = NULL;
 }
