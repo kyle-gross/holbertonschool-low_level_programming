cd6f12d14a5661aa12ada22f477bb879f9d17c7b
kyle-gross <kyg555@outlook.com>
2021-03-23 18:12:58 +0000

    change null check

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *prev, *temp;
 
-	if (!head)
+	if (!*head)
 		return (-1);
 	if (index >= listint_len(*head))
 		return (-1);
