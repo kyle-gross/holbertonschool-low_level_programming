76e1e060c2fd6d48ca33fb92a1f0841a3b720dc2
kyle-gross <kyg555@outlook.com>
2021-03-23 16:24:55 +0000

    Small change

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	if (*head == NULL)
+		return;
 	while (*head)
 	{
 		temp = *head;
-		*head = (*head)->next;
+		*head = temp->next;
 		free(temp);
 	}
 	*head = NULL;
