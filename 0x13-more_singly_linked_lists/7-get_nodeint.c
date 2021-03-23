ea4530289b43c93e852205f4609b5a743b569ed3
kyle-gross <kyg555@outlook.com>
2021-03-23 16:41:17 +0000

    remove commented code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,9 +18,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (temp_index < index)
 	{
 		temp = temp->next;
-/*		if (temp_index + 1 < index && temp->next == NULL)
-			return (NULL);
-*/		temp_index++;
+		temp_index++;
 	}
 	return (temp);
 }
