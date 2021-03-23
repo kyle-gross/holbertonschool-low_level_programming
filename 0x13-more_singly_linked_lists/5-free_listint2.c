15fcaa6d8bf7b06cdaffe6a80b551707fc6bd7a9
kyle-gross <kyg555@outlook.com>
2021-03-23 17:48:03 +0000

    Re format null check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,12 +8,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (!*head)
-		return;
-	while (*head)
-	{
-		temp = *head;
-		*head = temp->next;
-		free(temp);
+	if (*head)
+	{	
+		while (*head)
+		{
+			temp = *head;
+			*head = temp->next;
+			free(temp);
+		}
 	}
 }
