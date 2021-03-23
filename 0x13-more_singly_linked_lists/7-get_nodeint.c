2df9ec9254805354921fe0b3f25f216a844953df
kyle-gross <kyg555@outlook.com>
2021-03-23 06:08:00 +0000

    Change condition

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (temp_index < index)
 	{
 		temp = temp->next;
-		if (temp_index + 1 < index && temp->next == NULL)
+		if (temp_index < index && temp->next == NULL)
 			return (NULL);
 		temp_index++;
 	}
