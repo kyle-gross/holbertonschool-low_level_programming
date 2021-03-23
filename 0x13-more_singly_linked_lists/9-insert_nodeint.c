334bbe93f2087f2564ec1a38d05f9802555bb4d1
kyle-gross <kyg555@outlook.com>
2021-03-23 16:59:22 +0000

    Add several helper functions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,14 +15,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
-	new_node->n = n;
-	trav = *head;
-	while (--idx)
+	if (idx == 0)
+		new_node = add_nodeint(head, n);
+	else if (idx == listint_len(*head) - 1)
+		new_node = add_nodeint_end(head, n);
+	else
 	{
-		trav = trav->next;
+		new_node->n = n;
+		trav = *head;
+		while (--idx)
+		{
+			trav = trav->next;
+		}
+		new_node->next = trav->next;
+		trav->next = new_node;
 	}
-	new_node->next = trav->next;
-	trav->next = new_node;
 	return (new_node);
 }
 /**
@@ -43,4 +50,48 @@ size_t listint_len(const listint_t *h)
 	}
 	count++;
 	return (count);
+}
+/**
+ * add_nodeint_end - adds a node to the end of a list
+ * @head: pointer to head of list
+ * @n: new integer
+ * Return: new node or NULL if failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp, *traverse;
+
+	temp = malloc(sizeof(listint_t));
+	if (!temp)
+		return (NULL);
+	temp->n = n;
+	temp->next = NULL;
+	if (!*head)
+	{
+		*head = temp;
+		return (temp);
+	}
+	traverse = *head;
+	while (traverse->next)
+		traverse = traverse->next;
+	traverse->next = temp;
+	return (temp);
+}
+/**
+ * add_nodeint - adds a node to the beginning of a list
+ * @head: pointer to head of list
+ * @n: number to add to list
+ * Return: new node or NULL if fail
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
 }
