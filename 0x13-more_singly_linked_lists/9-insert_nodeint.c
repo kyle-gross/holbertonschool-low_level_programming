5ea1e67d4dcfc74185b6dcdcd537638b94883943
kyle-gross <kyg555@outlook.com>
2021-03-23 06:43:50 +0000

    undo changes

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,5 +30,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (temp_index = 0; temp_index < idx - 1; temp_index++)
 		trav = trav->next;
 	trav->next = new_node;
-	return (&new_node);
+	return (new_node);
 }
