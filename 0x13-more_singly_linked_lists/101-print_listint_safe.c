ccee4955a09edb2f34718262f62e616f64b0861c
kyle-gross <kyg555@outlook.com>
2021-03-24 21:26:32 +0000

    Update exit condition. idk why?

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -56,10 +56,10 @@ size_t print_listint_safe(const listint_t *head)
 
 	if (!head)
 		return (count);
+	if (!*head)
+		exit (98);
 	temp2 = cyclecheck(head);
 	temp = head;
-	if (!temp)
-		exit(98);
 	while (temp)
 	{
 		printf("[%p] %d\n", (void *)temp, temp->n);
