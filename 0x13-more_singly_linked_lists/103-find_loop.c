f0767d99f13d9f475aa14cbdc4f04cc63c21e225
kyle-gross <kyg555@outlook.com>
2021-03-24 03:02:42 +0000

    Change null check

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -8,16 +8,17 @@ listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow, *fast;
 
-	if (!head)
-		return (NULL);
-	slow = head;
-	fast = head;
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
-			return (slow);
+	if (head)
+	{	
+		slow = head;
+		fast = head;
+		while (slow && fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+			if (slow == fast)
+				return (slow);
+		}
 	}
 	return (NULL);
 }
