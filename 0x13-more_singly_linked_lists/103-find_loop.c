244e7db20ac4fb9311bf49637b6a441229db36b7
kyle-gross <kyg555@outlook.com>
2021-03-24 20:25:40 +0000

    Oops

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -17,7 +17,7 @@ listint_t *find_listint_loop(listint_t *head)
 		if (slow == fast)
 		{
 			fast = head;
-			while (slow != head)
+			while (slow != fast)
 			{
 				slow = slow->next;
 				fast = fast->next;
