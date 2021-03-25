60c39cd206ba2160cc54abf65514e1cad1ab7ac6
kyle-gross <kyg555@outlook.com>
2021-03-25 23:30:16 +0000

    Add () to return

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -23,5 +23,5 @@ unsigned int countSetBits(unsigned long int n)
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return countSetBits(n ^ m);
+	return (countSetBits(n ^ m));
 }
