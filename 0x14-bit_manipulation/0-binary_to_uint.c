9d16febde9640ab64fc8455cea3cfce6a09e4924
kyle-gross <kyg555@outlook.com>
2021-03-25 19:43:53 +0000

    Commit task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: the number to convert
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	for (; *b; b++)
+	{
+		if (*b == '1')
+			result = (result << 1) | 1;
+		else if (*b == '0')
+			result <<= 1;
+		else
+			break;
+	}
+	return (result);
+}
