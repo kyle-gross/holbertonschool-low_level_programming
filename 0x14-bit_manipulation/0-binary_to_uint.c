268720bbfe06c48dc945a294e55eda9c12790c75
kyle-gross <kyg555@outlook.com>
2021-03-25 19:51:07 +0000

    Redo 0/1 check

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,20 +6,14 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	const char *temp_b = b;
-	unsigned int result = 0, i, signal = 0;
+	unsigned int result = 0;
 
 	if (!b)
 		return (0);
-	for (i = 0; temp_b[i]; i++)
-	{
-		if (temp_b[i] < 48 || temp_b[i] > 49)
-			signal = 1;
-	}
-	if (signal == 1)
-		return (0);
 	for (; *b; b++)
 	{
+		if (*b < 48 || *b > 49)
+			return (0);
 		if (*b == '1')
 			result = (result << 1) | 1;
 		else if (*b == '0')
