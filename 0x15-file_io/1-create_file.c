8f825a7ef789813c9623e4286d94df8c6af6a2f3
kyle-gross <kyg555@outlook.com>
2021-03-30 20:10:07 +0000

    First push task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _strlen - string length
+ * @s: the string
+ * Return: count
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] && s)
+		count++;
+	return (count);
+}
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	len = _strlen(text_content);
+	write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
