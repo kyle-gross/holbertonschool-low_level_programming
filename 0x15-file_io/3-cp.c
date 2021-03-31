b6fa41a269c800dc83c4ef64ca5c1dce53cb4140
kyle-gross <kyg555@outlook.com>
2021-03-31 00:47:32 +0000

    Add -1 check for read

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,10 +11,7 @@ int main(int argc, char *argv[])
 	int fd, fd1, readval, closeval, writecheck;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
@@ -35,6 +32,11 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd), close(fd1), exit(99);
 		}
+	}
+	if (readval == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	closeval = close(fd);
 	if (closeval == -1)
