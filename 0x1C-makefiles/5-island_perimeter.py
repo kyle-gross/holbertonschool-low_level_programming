9f1d1490e814451b82f42c8f8f0e77ee571679d9
kyle-gross <kyg555@outlook.com>
2021-05-01 22:14:23 +0000

    Add tasks 0-5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+This function returns the perimeter of the island
+described in grid
+'''
+def island_perimeter(grid):
+    perim = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if grid[row - 1][col] == 0:
+                    perim += 1
+                if grid[row + 1][col] == 0:
+                    perim += 1
+                if grid[row][col - 1] == 0:
+                    perim += 1
+                if grid[row][col + 1] == 0:
+                    perim += 1
+    return perim
