d1a3df6e5b830dc9bbf0b611c01cd93a6e28ee62
kyle-gross <kyg555@outlook.com>
2021-05-02 18:57:40 +0000

    Fix pep

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""
-This function returns the perimeter of the island
-described in grid
-"""
 def island_perimeter(grid):
+    """
+    This function  returns the perimeter of the island
+    found in grid
+    """
     perim = 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
