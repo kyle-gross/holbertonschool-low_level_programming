aeb6cf486beac80e74c044adea64aab9d490c0eb
kyle-gross <kyg555@outlook.com>
2021-05-02 20:33:40 +0000

    Change # to """

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,17 +7,23 @@ grid: matrix of 0's and 1's
 
 def island_perimeter(grid):
     """ Let's find the perimeter of an island! :D """
-    # Null check
+
+    """ Null check """
     if grid is None:
         return 0
+
+    """ VARIABLES """
     perim = 0
     row_len = len(grid)
     col_len = len(grid[0])
-    # Verify that grid is right size
+
+    """ Verify that grid is right size """
     if row_len > 100 or col_len > 100:
         return 0
-    # Loop through grid and look around when 1's are encountered
-    # If there's 0's or matrix edge around a 1, perimeter increases
+
+    """ Loop through grid and look around when 1's are encountered
+     If there's 0's or matrix edge around a 1, perimeter increases
+    """
     for row in range(row_len):
         for col in range(col_len):
             if grid[row][col] == 1:
