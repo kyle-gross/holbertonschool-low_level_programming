12de4a14187d061f3d1f6ee7f45f933d9470dcf9
kyle-gross <kyg555@outlook.com>
2021-05-02 19:51:07 +0000

    Add some documentation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""
+This function returns the perimeter of an island
+1 represents land and 0 represents water
+grid: matrix of 0's and 1's
+Return: perimeter of island
+"""
+
+
 def island_perimeter(grid):
-    # This function  returns the perimeter of the island
-    # found in grid
     perim = 0
+    # Loop through grid and look around when 1's are encountered
+    # If there's 0's or matrix edge around a 1, perimeter increases
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
