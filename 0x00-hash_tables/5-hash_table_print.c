297c3e60700fc7c0715d95fa879f101f75b3878a
kyle-gross <kyg555@outlook.com>
2021-07-28 21:40:26 +0000

    Add task 5

---
+++ 0x00-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p;
+	char n = 0;
+
+	printf("{");
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p)
+		{
+			if (n == 1)
+				printf(", ");
+			printf("'%s': '%s'", p->key, p->value);
+			p = p->next;
+			n = 1;
+		}
+	}
+	printf("}\n");
+}
