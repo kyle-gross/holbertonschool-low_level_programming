4100dcd2a61c32e53e03607f47c3c27cfa854457
kyle-gross <kyg555@outlook.com>
2021-07-28 19:17:58 +0000

    Add index variable to array in loop

--- 0x00-hash_tables/0-hash_table_create.c
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -24,7 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		hash_table->array = NULL;
+		hash_table->array[i] = NULL;
 
 	hash_table->size = size;
 
