fcc5ead4d484b092ae67ef02671056aca0016620
kyle-gross <kyg555@outlook.com>
2021-07-28 21:25:05 +0000

    Add task 4

---
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets a value associated with a key
+ * @ht: hash table
+ * @key: key to look for
+ * Return: value associeted with element, or NULL if not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int bin = 0;
+	hash_node_t *pair;
+
+	if (!ht)
+		return (0);
+	bin = key_index((const unsigned char *)key, ht->size);
+
+	pair = ht->array[bin];
+	while (pair && pair->key && strcmp(key, pair->key) != 0)
+		pair = pair->next;
+
+	if (!pair || !pair || strcmp(key, pair->key) != 0)
+		return NULL;
+	else
+		return pair->value;
+}
