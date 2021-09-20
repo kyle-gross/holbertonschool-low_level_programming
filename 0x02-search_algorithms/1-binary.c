caf0ff0bd33cb09786940a44511bb00ee9cae528
Kyle <kyg555@outlook.com>
2021-09-20 12:09:36 -0700

    Fix tabs

--- 0x02-search_algorithms/1-binary.c
+++ 0x02-search_algorithms/1-binary.c
@@ -11,14 +11,14 @@
  */
 void print_array(const int *array, int start, int end)
 {
-    int i;
+	int i;
 
 	printf("Searching in array: %d", array[start]);
-    for (i = start + 1; i <= end; i++)
-    {
-        printf(", %d", array[i]);
-    }
-    printf("\n");
+	for (i = start + 1; i <= end; i++)
+	{
+		printf(", %d", array[i]);
+	}
+	printf("\n");
 }
 
 /**
